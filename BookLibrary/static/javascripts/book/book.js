74af353583b070f3b89beafaad707322a6567b36
Mrson <rw_Song@sina.com>
2015-10-28 23:43:59 +0800

    图书详情获取完成

--- BookLibrary/static/javascripts/book/book.js
+++ BookLibrary/static/javascripts/book/book.js
@@ -6,51 +6,49 @@ var app = app || {};
 
 //  每本书的详情模型
 app.BookDetailModel = Backbone.Model.extend({
-    "urlRoot": '/api/book',
-    "defaults":{
-        "coverImage":"images/default.jpg",
-        "title":"No title",
-        "author":"Unknown",
-        "releaseDate":"Unknown",
-        "keywords":"None",
-        "bookId":"id"
+    "defaults": {
+        "coverImage": "images/default.jpg",
+        "title": "No title",
+        "author": "Unknown",
+        "releaseDate": "Unknown",
+        "keywords": "None",
+        "bookId": "id"
     },
-    "parse":function(res){
+    "parse": function (res) {
         res.id = res._id;
         return res;
+    },
+    "fetch": function (id) {
+        var self = this, tmpNews;
+        $.ajax({
+            "url": "/api/book/" + id,
+            "type": "GET",
+            "success": function (res) {
+                self.set({
+                    "coverImage":res[0]["coverImage"],
+                    "title":res[0]["title"],
+                    "author":res[0]["author"],
+                    "releaseDate":res[0]["releaseDate"],
+                    "keywords":res[0]["keywords"],
+                    "bookId":res[0]["bookId"]
+                });
+                self.trigger("book:Detail");
+            }
+        });
     }
-});
-
-//  一本书的集合
-app.BookLibrary = Backbone.Collection.extend({
-    "model":app.BookDetailModel,
-    "url":"/api/books"
 });
 
 //  图书详情
 app.BookDetailView = Backbone.View.extend({
-    "el":"#book-detail",
+    "el": "#book-detail",
+    "model": new app.BookDetailModel(),
     "template": _.template($("#bookDetailTemplate").html()),
     "initialize": function (id) {
-        var _this = this,
-            Book = new app.BookDetailModel({
-            "id":id
-        });
-        this.collection = new app.BookLibrary({
-            "id":id
-        });
-        this.collection.fetch({
-            "reset":true
-        });
-        Book.fetch({
-            reset:true,
-            success: function (book) {
-                _this.render(book);
-            }
-        });
+        this.model.fetch(id);
+        this.model.bind("book:Detail", this.render, this);
     },
-    "render":function(model){
-        this.$el.html(this.template(model.toJSON()));
+    "render": function () {
+        this.$el.html(this.template(this.model.toJSON()));
         return this;
     }
 });
