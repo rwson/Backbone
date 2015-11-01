141f772a4c3a15935ecf1aa6346c148576731dcc
Mrson <rw_Song@sina.com>
2015-11-01 20:14:42 +0800

    html、js、server端部分

--- backboneChart/app.js
+++ backboneChart/app.js
@@ -12,15 +12,17 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 app.use(express.favicon());
+app.use(express.session({
+    secret:"backboneChat",
+    cookie:{maxAge:1000 * 60 * 60 * 24 * 30}
+}));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 // development only
 if ('development' == app.get('env')) {
@@ -31,4 +33,5 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+//  apply routers
 routes(app);
--- backboneChart/routes/index.js
+++ backboneChart/routes/index.js
@@ -3,6 +3,7 @@
  */
 
 var Models = require("../models").Models;
+var crypto = require('crypto');
 
 module.exports = function (app) {
 
@@ -10,19 +11,31 @@ module.exports = function (app) {
      * 用户注册
      */
     app.post("/user", function (req, res) {
-        var user = new Models.User({
+        var User = new Models.User({
             "id": _randomId(),
-            "username": req.body.name,
-            "password": req.body.password,
+            "username": req.body.username,
+            "password": _encryptString(req.body.password),
             "register_time": Date.now()
         });
-        user.save(function(err,user){
-            if(err){
-                return res.send(500,err);
+        Models.User.find({
+            "username": req.body.username
+        }, function (err, user) {
+            if (err) {
+                return res.send(500, err);
+            } else if (user.length) {
+                return res.send(400, {
+                    "user": user,
+                    "message": "用户名已存在!"
+                });
             }
-            res.send(200,{
-                "code":200,
-                "user":user
+            User.save(function (err, user) {
+                if (err) {
+                    return res.send(500, err);
+                }
+                res.send(200, {
+                    "code": 200,
+                    "user": user
+                });
             });
         });
     });
@@ -31,13 +44,13 @@ module.exports = function (app) {
      * 获取用户列表
      */
     app.get("/user", function (req, res) {
-        Models.User.find(function(err,users){
-            if(err){
-                return res.send(500,err);
+        Models.User.find(function (err, users) {
+            if (err) {
+                return res.send(500, err);
             }
-            res.send(200,{
-                "code":200,
-                "users":users
+            res.send(200, {
+                "code": 200,
+                "users": users
             });
         });
     });
@@ -47,10 +60,11 @@ module.exports = function (app) {
      */
     app.get("/user/:id", function (req, res) {
         Models.User.find({
-            "id":req.body.id
-        },function(err,user){
-            if(err){
-
+            "id": req.body.id
+        }, function (err, user) {
+            if (err) {
+                console.log(err);
+                return res.send(500, err);
             }
         });
     });
@@ -59,24 +73,84 @@ module.exports = function (app) {
      * 用户登录
      */
     app.post("/login", function (req, res) {
+        Models.User.find({
+            "password": _encryptString(req.body.password)
+        }, function (err, user) {
+            if (err) {
+                console.log(err);
+                return res.send(500, err);
+            } else if (!user.length) {
+                res.send(400, {
+                    "message": "登录失败!密码错误!"
+                });
+            } else if (user[0].username == req.body.username) {
+                req.session.user = user[0];
+                res.send(200, user[0]);
+            } else {
+                res.send(400, {
+                    "message": "登录失败!用户名错误!"
+                });
+            }
+        });
     });
 
     /**
      * 用户登出
      */
     app.get("/logout", function (req, res) {
+        req.session.user = null;
+        res.send(200,{
+            "message":"登出成功!"
+        });
     });
 
     /**
      * 话题列表
      */
     app.get("/topic", function (req, res) {
+        Models.Topic.find(function (err, topics) {
+            if (err) {
+                console.log(err);
+                return res.send(500, err);
+            }
+            res.send(200, {
+                "topics": topics
+            });
+        });
     });
 
     /**
      * 新建话题
      */
     app.post("/topic", function (req, res) {
+        console.log(req.session);
+        var Topic = new Models.Topic({
+            "id": _randomId(),
+            "title": req.body.title,
+            "create_time": Date.now(),
+            "owner": req.session.user["id"]
+        });
+        Models.Topic.find({
+            "title": req.body.title
+        }, function (err, topic) {
+            if (err) {
+                console.log(err);
+                res.send(500, err);
+            } else if (topic.length) {
+                res.send(400, {
+                    "message":"该话题已存在!"
+                });
+            }else{
+                Topic.save(function(err,topic){
+                    if(err){
+                        console.log(err);
+                        res.send(200,{
+                            "message":"话题创建成功!"
+                        });
+                    }
+                });
+            }
+        });
     });
 
     /**
@@ -103,6 +177,10 @@ module.exports = function (app) {
     app.delete("/message/:id", function (req, res) {
     });
 
+    app.use(function (req, res) {
+        res.sendfile("static/index.html");
+    });
+
 };
 
 /**
@@ -112,7 +190,19 @@ module.exports = function (app) {
  */
 function _randomId() {
     var S4 = function () {
-        return Math.floor(Math.random() *  0x10000).toString(16);
+        return Math.floor(Math.random() * 0x10000).toString(16);
     };
-    return [S4(),S4(),S4(),S4(),S4(),S4(),S4(),S4()].join("");
+    return [S4(), S4(), S4(), S4(), S4(), S4(), S4(), S4()].join("");
+}
+
+/**
+ * 加密字符串
+ * @param str   被加密的字符串
+ * @return {string}
+ * @private
+ */
+function _encryptString(str) {
+    var sha1 = crypto.createHash("sha1");
+    sha1.update("" + str);
+    return sha1.digest("hex");
 }
